073d4e0af438ada21a4e914a044a2717531a4a75
trimikpro <vupotiroy112@gmail.com>
2025-08-13 15:46:37 +0600

    conditional rendaring state:   style and button toggle

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,27 @@
- import React from 'react';
+ import React, { useState } from 'react';
  import './Country.css'
  
 const Country = ({ countryValue }) => {
     const { name, flags } = countryValue;
-    console.log(countryValue)
+    
+    const [visited, setVisited] = useState(false);
+    //console.log(!visited);
+
+    const handleVisited =()=>{
+        setVisited(!visited);
+       
+    }
+    
+    
     return (
         <>
             <div className='country'>
                 <img className='flagsImg' src={flags.png} alt="" />
                 <h3 >Name : {name?.common}</h3>
                 <p>official name: {name?.official}</p>
+
+                <button onClick={handleVisited}>{visited ? 'visited': 'going'} </button>
+                <p>{visited ? <p className='visT'>this country visited</p> : <p className='visF'>I want visit</p>}</p>
             </div>
         </>
     );
